c9fd0546378ac8e1620bad01c4768d78fd70ef3f
regenerativep <regeneralp@gmail.com>
2023-04-12 19:37:19 -0400

    and a build.zig

---
+++ build.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    //const target = b.standardTargetOptions(.{});
+    //const optimize = b.standardOptimizeOption(.{});
+    _ = b.addModule("extrapacked", .{ .source_file = .{ .path = "extrapacked.zig" } });
+}
