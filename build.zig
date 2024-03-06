872be985e359c68bd7473a7b87b80ce80306765d
regenerativep <regeneralp@gmail.com>
2024-03-05 22:42:32 -0500

    fix build

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    //const target = b.standardTargetOptions(.{});
-    //const optimize = b.standardOptimizeOption(.{});
-    _ = b.addModule("extrapacked", .{ .source_file = .{ .path = "src/extrapacked.zig" } });
+pub fn build(b: *std.Build) void {
+    _ = b.addModule("extrapacked", .{
+        .root_source_file = .{ .path = "src/extrapacked.zig" },
+    });
 }
